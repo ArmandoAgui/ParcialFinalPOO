461c6e97ffaefeff502c98c77327d0dcef616818
José Luis Milán <00144723@uca.edu.sv>
2024-07-06 17:33:22 -0600

    :memo: add: funcionalidad de generar un reporte txt

--- ParcialFinalPOO/src/main/java/org/example/parcialfinalpoo/HelloController.java
+++ ParcialFinalPOO/src/main/java/org/example/parcialfinalpoo/HelloController.java
@@ -7,6 +7,10 @@ import org.example.parcialfinalpoo.Clases.Cliente;
 import org.example.parcialfinalpoo.Clases.Tarjeta;
 import org.example.parcialfinalpoo.Clases.Transaccion;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.*;
 import java.time.LocalDate;
 
@@ -28,7 +32,7 @@ public class HelloController {
     @FXML
     private TextField clientCardField; //00116223 -> Se le designa el nombre de los elementos de JavaFX dentro del controller.'
     @FXML
-    private ListView<Transaccion> tarjetasListView; //00116223 -> Se le asigna el nombre del elemento de JavaFX dentro del controller. Se encarga de presentar las tarjetas, con su informacion al momento de modificarla su informacion.
+    private ListView<String> tarjetasListView; //00116223 -> Se le asigna el nombre del elemento de JavaFX dentro del controller. Se encarga de presentar las tarjetas, con su informacion al momento de modificarla su informacion.
     @FXML
     private TextField deletedNameTextField; //00116223 -> Se le asigna el nombre del elemento de JavaFX dentro del controller. Elemento utilizado dentro de la eliminacion de tarjetas
     @FXML
@@ -74,8 +78,8 @@ public class HelloController {
                 //00144723 - Aqui establecemos conexion a la BD
                 Connection conn = DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/parcialfinal",
-                        "root",
-                        "jlmilan");
+                        "sa",
+                        "123456");
                 //00144723 Aqui preparamos una sentencia para realizar la consulta a la BD
                 PreparedStatement psMostrarCompras = conn.prepareStatement("select c.nombreCompleto as Nombre, c.direccion as Domicilio, c.telefono as Telefono,\n" +
                         "       T.descripcion as Descripcion, T.montoTotal as MontoCompra, T.fecha as FechaCompra,\n" +
@@ -94,45 +98,60 @@ public class HelloController {
                 //00144723 - Ejecutamos la consulta
                 ResultSet rs = psMostrarCompras.executeQuery();
 
-                // 00144723 - Limpiamos el listview por si aun tiene datos almacenados ya que nosotros queremos mostrar nuestro reporte
-                // y asi evitar errores.
-                tarjetasListView.getItems().clear();
-
-                //00144723 - Aqui iremos guardando los datos del cliente y de la transaccion realizada para poder mostrarlos posteriormente
-                while (rs.next()) {
-                    String nombre = rs.getString("Nombre");
-                    String direccionCliente = rs.getString("Domicilio");
-                    String telefonoCliente = rs.getString("Telefono");
-
-                    Cliente cliente = new Cliente(nombre, direccionCliente, telefonoCliente);
-
-
-                    String numTarjeta = rs.getString("Tarjeta");
-                    String Facilitador = rs.getString("Facilitador");
-                    String fechaExpiracion = rs.getString("FechaExpiracion");
-                    String tipo = rs.getString("Tipo");
-
-                    Tarjeta tarjeta = new Tarjeta(numTarjeta, fechaExpiracion, tipo, Facilitador, cliente);
-
-                    String descripcion = rs.getString("Descripcion");
-                    double montoTotal = rs.getDouble("MontoCompra");
-                    Date fechaCompra = rs.getDate("FechaCompra");
-
-                    Transaccion transaccion = new Transaccion(fechaCompra, montoTotal, descripcion, tarjeta);
-                    tarjetasListView.getItems().add(transaccion);
-
-
+                //00144723 - Se crea un carpeta si no existe, esto con el fin de ir almacenando los reportes
+                File reporteA = new File("ReporteA");
+                if (!reporteA.exists()) {
+                    reporteA.mkdirs();//00144723 - Si no existe se crea
                 }
+                //00144723 - Se guarda el nombre que se desea tener en nuestro caso designamos este formato.
+                String nombreArchivo = "Reporte A" + " - " + java.time.LocalDateTime.now().toString().replace(':', '-');
 
-                conn.close();
+                //00144723 - Se crea un objeto BufferedWriter para poder ir escribiendo dentro de la carpeta que almacenará los txt
+                try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(reporteA, nombreArchivo + ".txt")))) {
+                    //00144723 - Aqui se escribe este mensaje como encabezado
+                    bufferedWriter.write("Reporte de compras que ha realizado el cliente con ID: " + idEmpleado + "\n\n");
+                    //00144723 - Aqui iremos guardando los datos del cliente y de la transaccion realizada para poder mostrarlos posteriormente
+                    while (rs.next()) {
+                        String nombre = rs.getString("Nombre"); //00144723 - se extrae el nombre de usuario
+                        String direccionCliente = rs.getString("Domicilio"); //00144723 - se extrae el domicilio de usuario
+                        String telefonoCliente = rs.getString("Telefono");//00144723 - se extrae el telefono de usuario
+                        String numTarjeta = rs.getString("Tarjeta");//00144723 - se extrae el numero de tarjeta
+                        String Facilitador = rs.getString("Facilitador");//00144723 - se extrae el nombre del facilitador
+                        String fechaExpiracion = rs.getString("FechaExpiracion");//00144723 - se extrae la fecha de expiracion                        String tipo = rs.getString("Tipo");
+                        String tipo = rs.getString("Tipo");//00144723 - Se extrae el tipo de tarjeta
+                        String descripcion = rs.getString("Descripcion"); //00144723 - Se la descripcion de la compra
+                        double montoTotal = rs.getDouble("MontoCompra");//00144723 - Se extrae el monto de la compra
+                        Date fechaCompra = rs.getDate("FechaCompra");//00144723 - Se extrae la fecha de la compra
+
+
+                        bufferedWriter.write("\n");  //00144723 - Se escribe un salto de linea
+                        bufferedWriter.write("Cliente: " + nombre + "\n");//00144723 - Se escribe el nombre del cliente
+                        bufferedWriter.write("Fecha de compra: " + fechaCompra + "\n");//00144723 - Se escribe la fecha de compra
+                        bufferedWriter.write("Monto total de la compra: " + montoTotal + "\n");//00144723 - Se escribe el monto de la compra
+                        bufferedWriter.write("Numero de tarjeta: " + numTarjeta + "\n");//00144723 - Se escribe el numero de la tarjeta
+                        bufferedWriter.write("Fecha de expiracion: " + fechaExpiracion + "\n");//00144723 - Se escribe la fecha de expiracio de la tarjeta
+                        bufferedWriter.write("Tipo: " + tipo + "\n");//00144723 - Se escribe el tipo de tarjeta
+                        bufferedWriter.write("Facilitador: " + Facilitador + "\n");//00144723 - Se escribe el facilitador
+                    }
+                } catch (IOException i) {
+                    mostrarAlerta("Error", "Error al escribir en el archivo: " + i.getMessage()); //00144723 - Se muestra una alerta si se generá un error
+                }
+                conn.close(); //00144723 - Como toda buena practica de un estudiante UCA se cierra la conexion
 
 
             } catch (Exception e) {
                 //00144723 - Aqui mostramos un mensaje de error y la excepcion que se salió pero en forma de mensaje
-                System.out.println("Error al conectar la base de datos.");
-                System.out.println(e.getMessage());
+                mostrarAlerta("ERROR!", e.getMessage());
             }
         }
 
+    }
+    //00144723 - Se crea un metodo para mostrar una alerta
+    private void mostrarAlerta(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION); // 00144723 - se crea una alerta de informacion
+        alert.setTitle(titulo); // 00144723 - se le asigna un titulo
+        alert.setHeaderText(null); // 00144723 - se le elimina el encabezado a la alerta
+        alert.setContentText(mensaje); // 00144723 - se le asigna un mensaje que deberá mostrar
+        alert.showAndWait(); //00144723 - muestra la alerta y se cerrará hasta que el usuario lo decida
     }
 }
