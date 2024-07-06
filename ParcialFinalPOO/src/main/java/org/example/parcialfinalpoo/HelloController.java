68facc65b567c92e55321adbbb099441ba38478e
Herberth Chavez <00116223@uca.edu.sv>
2024-07-05 19:10:49 -0600

    :construction: Asignacion de elementos dentro de controller

--- ParcialFinalPOO/src/main/java/org/example/parcialfinalpoo/HelloController.java
+++ ParcialFinalPOO/src/main/java/org/example/parcialfinalpoo/HelloController.java
@@ -2,13 +2,23 @@ package org.example.parcialfinalpoo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private TextField nameField; //00116223 -> Se le designa el nombre de los elementos de JavaFX dentro del controller.
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TextField adressField; //00116223 -> Se le designa el nombre de los elementos de JavaFX dentro del controller.
+    @FXML
+    private TextField phoneField; //00116223 -> Se le designa el nombre de los elementos de JavaFX dentro del controller.
+    @FXML
+    private TextField numberCardField; //00116223 -> Se le designa el nombre de los elementos de JavaFX dentro del controller.
+    @FXML
+    private TextField dateCardField; //00116223 -> Se le designa el nombre de los elementos de JavaFX dentro del controller.
+    @FXML
+    private TextField typeCardField; //00116223 -> Se le designa el nombre de los elementos de JavaFX dentro del controller.
+    @FXML
+    private TextField companyCardField; //00116223 -> Se le designa el nombre de los elementos de JavaFX dentro del controller.
+    @FXML
+    private TextField clientCardField; //00116223 -> Se le designa el nombre de los elementos de JavaFX dentro del controller.
 }
