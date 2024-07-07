536e80ad1449d60526afd623494e39d4a4ca4f48
ArmandoAgui <128849306+ArmandoAgui@users.noreply.github.com>
2024-07-06 18:25:01 -0600

    :bug: merge fallido

--- ParcialFinalPOO/src/main/java/org/example/parcialfinalpoo/Clases/Cliente.java
+++ ParcialFinalPOO/src/main/java/org/example/parcialfinalpoo/Clases/Cliente.java
@@ -1,14 +1,12 @@
 package org.example.parcialfinalpoo.Clases;
 
 public class Cliente {
-    private int id;
-    private String nombreCompleto;
+    private String nombre;
     private String direccion;
     private String telefono;
 
-    public Cliente(int id, String nombreCompleto, String direccion, String telefono) {
-        this.id = id;
-        this.nombreCompleto = nombreCompleto;
+    public Cliente(String nombre, String direccion, String telefono) {
+        this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
     }
@@ -16,20 +14,27 @@ public class Cliente {
     public Cliente() {
     }
 
-    public int getId() {
-        return id;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
-    public String getNombreCompleto() { return nombreCompleto; }
-    public void setNombreCompleto(String nombreCompleto) { this.nombreCompleto = nombreCompleto; }
+    public String getDireccion() {
+        return direccion;
+    }
 
-    public String getDireccion() { return direccion; }
-    public void setDireccion(String direccion) { this.direccion = direccion; }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 
-    public String getTelefono() { return telefono; }
-    public void setTelefono(String telefono) { this.telefono = telefono; }
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
