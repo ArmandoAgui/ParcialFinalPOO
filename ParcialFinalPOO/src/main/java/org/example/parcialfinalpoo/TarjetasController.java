5e085cb0a1fff2d2a56eb382dc56c6c2759a17d7
ArmandoAgui <128849306+ArmandoAgui@users.noreply.github.com>
2024-07-08 17:53:56 -0600

    :bug: Feat:Solucion bug de table view

--- ParcialFinalPOO/src/main/java/org/example/parcialfinalpoo/TarjetasController.java
+++ ParcialFinalPOO/src/main/java/org/example/parcialfinalpoo/TarjetasController.java
@@ -3,39 +3,24 @@ package org.example.parcialfinalpoo;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import org.example.parcialfinalpoo.Clases.Cliente;
 import org.example.parcialfinalpoo.Clases.Tarjeta;
 
 import java.net.URL;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
-public class TarjetasController {
-    //Campos ingreso de datos.
-    @FXML
-    private TextField txtNoCompleto;
-    @FXML
-    private TextField txtCaducidad;
-    @FXML
-    private ComboBox comboBoxTipo;
-    @FXML
-    private ComboBox comboBoxFacilitador;
-    @FXML
-    private TextField idClienteTarjetas;
+public class TarjetasController implements Initializable {
 
-    //Mostrar base de datos tarjetas
     @FXML
     private TableView<Tarjeta> tblTarjetas;
     @FXML
     private TableColumn<Tarjeta, Integer> colId;
-
-    /**
-     * Checkout
-     **/
     @FXML
-    private TableColumn<Cliente, String> colCliente;
+    private TableColumn<Tarjeta, String> colCliente;
     @FXML
     private TableColumn<Tarjeta, String> colNoTarjeta;
     @FXML
@@ -44,14 +29,18 @@ public class TarjetasController {
     private TableColumn<Tarjeta, String> colFechaCaducidad;
     @FXML
     private TableColumn<Tarjeta, String> colTipo;
-
-    /**
-     * Checkout
-     **/
-    /*@FXML
-    private TableColumn<Tarjeta, String> colDui;*/
-
-    //Botones
+    @FXML
+    private TextField txtNoCompleto;
+    @FXML
+    private ComboBox<String> comboBoxTipo;
+    @FXML
+    private ComboBox<String> comboBoxFacilitador;
+    @FXML
+    private TextField txtBuscarIDTarjeta;
+    @FXML
+    private TextField txtFechaCaducidad;
+    @FXML
+    private TextField txtClienteId;
     @FXML
     private Button btnAgregar;
     @FXML
@@ -63,21 +52,18 @@ public class TarjetasController {
     @FXML
     private Button btnBuscar;
 
-    @FXML
-    private TextField txtBuscarIDTarjeta;
-
-    private static final String jdbcUrl = "jdbc:mysql://localhost:3306/parcialFinal";
-    private static final String usuario = "root";
-    private static final String contrasena = "root1234";
-
     private ObservableList<Tarjeta> listaTarjetas;
+
+    private final String jdbcUrl = "jdbc:mysql://localhost:3306/parcialFinal";
+    private final String usuario = "sa";
+    private final String contrasena = "12345678";
 
     public void initialize(URL location, ResourceBundle resources) {
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        colCliente.setCellValueFactory(new PropertyValueFactory<>("clienteID"));
-        colNoTarjeta.setCellValueFactory(new PropertyValueFactory<>("numeroTarjeta"));
-        colFacilitador.setCellValueFactory(new PropertyValueFactory<>("facilitadorID"));
-        colFechaCaducidad.setCellValueFactory(new PropertyValueFactory<>("fechaExpiracion"));
+        colCliente.setCellValueFactory(new PropertyValueFactory<>("clienteId"));
+        colNoTarjeta.setCellValueFactory(new PropertyValueFactory<>("NoTarjeta"));
+        colFacilitador.setCellValueFactory(new PropertyValueFactory<>("facilitador"));
+        colFechaCaducidad.setCellValueFactory(new PropertyValueFactory<>("fechaCaducidad"));
         colTipo.setCellValueFactory(new PropertyValueFactory<>("tipo"));
 
         listaTarjetas = FXCollections.observableArrayList();
@@ -89,7 +75,8 @@ public class TarjetasController {
         btnActualizar.setOnAction(event -> actualizarTarjeta());
         btnEliminar.setOnAction(event -> eliminarTarjeta());
         btnLimpiar.setOnAction(event -> limpiarCampos());
-        btnBuscar.setOnAction(even -> buscarTarjetaPorId());
+        btnBuscar.setOnAction(event -> buscarTarjetaPorId());
+
         tblTarjetas.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
                 cargarDatosCliente(newValue);
@@ -97,98 +84,10 @@ public class TarjetasController {
         });
     }
 
-    private void agregarTarjeta() {
-        String numeroTarjeta = txtNoCompleto.getText();
-        String fechaCaducidad = txtCaducidad.getText();
-        String tipo = comboBoxTipo.getValue().toString();
-        String facilitador = comboBoxFacilitador.getValue().toString();
-        String idCliente = idClienteTarjetas.getText();
-
-        String consultaSQL = "INSERT INTO TARJETAS (numeroTarjeta, fechaExpiracion, tipo, facilitadorId,clienteId) VALUES (?, ?, ?, ?,?)";
-
-        try (Connection conn = DriverManager.getConnection(jdbcUrl, usuario, contrasena);
-             PreparedStatement statement = conn.prepareStatement(consultaSQL)) {
-
-            statement.setString(1, numeroTarjeta);
-            statement.setString(2, fechaCaducidad);
-            statement.setString(3, tipo);
-            statement.setString(4, facilitador);
-            statement.setString(5, idCliente);
-
-            statement.executeUpdate();
-            cargarTarjetas();
-            limpiarCampos();
-        } catch (SQLException e) {
-            mostrarAlerta("Error", "Error al agregar cliente: " + e.getMessage());
-        }
-    }
-
-    private void actualizarTarjeta() {
-        Tarjeta tarjetaSeleccionada = tblTarjetas.getSelectionModel().getSelectedItem();
-
-        if (tarjetaSeleccionada == null) {
-            mostrarAlerta("Error", "Debe seleccionar un cliente para actualizar.");
-            return;
-        }
-
-        String numeroTarjeta = txtNoCompleto.getText();
-        String fechaCaducidad = txtCaducidad.getText();
-        String tipo = comboBoxTipo.getValue().toString();
-        String facilitador = comboBoxFacilitador.getValue().toString();
-        String idCliente = idClienteTarjetas.getText();
-
-        String consultaSQL = "UPDATE TARJETAS SET numeroTarjeta = ?, fechaExpiracion = ?, tipo = ?, facilitadorID = ?, clienteId = ? WHERE id = ?";
-
-        try (Connection conexion = DriverManager.getConnection(jdbcUrl, usuario, contrasena);
-             PreparedStatement statement = conexion.prepareStatement(consultaSQL)) {
-
-            statement.setString(1, numeroTarjeta);
-            statement.setString(2, fechaCaducidad);
-            statement.setString(3, tipo);
-            statement.setString(4, facilitador);
-            statement.setString(5, idCliente);
-            statement.setInt(6, tarjetaSeleccionada.getIdCliente());
-
-            statement.executeUpdate();
-            cargarTarjetas();
-            limpiarCampos();
-
-            btnActualizar.setDisable(true);
-            btnEliminar.setDisable(true);
-            btnAgregar.setDisable(false);
-        } catch (SQLException e) {
-
-            mostrarAlerta("Error", "Error al actualizar cliente: " + e.getMessage());
-        }
-    }
-
-    private void eliminarTarjeta() {
-        Tarjeta tarjetaSeleccionada = tblTarjetas.getSelectionModel().getSelectedItem();
-
-        if (tarjetaSeleccionada == null) {
-            mostrarAlerta("Error", "Debe seleccionar una tarjeta para eliminar.");
-            return;
-        }
-
-        String consultaSQL = "DELETE FROM Tarjeta WHERE id = ?";
-
-        try (Connection conn = DriverManager.getConnection(jdbcUrl, usuario, contrasena);
-             PreparedStatement statement = conn.prepareStatement(consultaSQL)) {
-
-            statement.setInt(1, tarjetaSeleccionada.getIdCliente());
-            statement.executeUpdate();
-            cargarTarjetas();
-            limpiarCampos();
-            btnActualizar.setDisable(true);
-            btnEliminar.setDisable(true);
-            btnAgregar.setDisable(false);
-        } catch (SQLException e) {
-            mostrarAlerta("Error", "Error al eliminar cliente: " + e.getMessage());
-        }
-    }
-
     private void cargarTarjetas() {
-        String consultaSQL = "SELECT * FROM TARJETAS c";
+        String consultaSQL = "SELECT t.id, t.numeroTarjeta AS NoTarjeta, t.fechaExpiracion AS fechaCaducidad, t.tipo AS TipoTarjeta, f.nombre AS Facilitador, t.clienteId " +
+                "FROM TARJETAS t " +
+                "INNER JOIN Facilitadores f ON t.facilitadorId = f.id";
 
         try (Connection conn = DriverManager.getConnection(jdbcUrl, usuario, contrasena);
              Statement statement = conn.createStatement();
@@ -198,11 +97,12 @@ public class TarjetasController {
 
             while (resultado.next()) {
                 Tarjeta tarjeta = new Tarjeta(
-                        resultado.getString("numeroTarjeta"),
-                        resultado.getString("fechaExpiracion"),
-                        resultado.getString("tipo"),
-                        resultado.getString("facilitadorId"),
-                        resultado.getInt("clienteId")
+                        resultado.getInt("id"),
+                        resultado.getString("clienteId"),
+                        resultado.getString("NoTarjeta"),
+                        resultado.getString("Facilitador"),
+                        resultado.getString("fechaCaducidad"),
+                        resultado.getString("TipoTarjeta")
                 );
                 listaTarjetas.add(tarjeta);
             }
@@ -211,61 +111,152 @@ public class TarjetasController {
         }
     }
 
-    private void buscarTarjetaPorId() {
-        String idBuscado = txtBuscarIDTarjeta.getText();
-        if (idBuscado.isEmpty()) {
+    private void agregarTarjeta() {
+        String numeroTarjeta = txtNoCompleto.getText();
+        String fechaExpiracion = txtFechaCaducidad.getText();
+        String tipo = comboBoxTipo.getSelectionModel().getSelectedItem();
+        String facilitador = comboBoxFacilitador.getSelectionModel().getSelectedItem();
+        int clienteId = Integer.parseInt(txtClienteId.getText());
+
+        String consultaSQL = "INSERT INTO Tarjetas (numeroTarjeta, fechaExpiracion, tipo, facilitadorId, clienteId) " +
+                "VALUES (?, ?, ?, (SELECT id FROM Facilitadores WHERE nombre = ?), ?)";
+
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, usuario, contrasena);
+             PreparedStatement statement = conn.prepareStatement(consultaSQL)) {
+
+            statement.setString(1, numeroTarjeta);
+            statement.setString(2, fechaExpiracion);
+            statement.setString(3, tipo);
+            statement.setString(4, facilitador);
+            statement.setInt(5, clienteId);
+            statement.executeUpdate();
+
+            mostrarAlerta("Éxito", "Tarjeta agregada exitosamente.");
             cargarTarjetas();
+            limpiarCampos();
+        } catch (SQLException e) {
+            mostrarAlerta("Error", "Error al agregar tarjeta: " + e.getMessage());
+        }
+    }
+
+    private void actualizarTarjeta() {
+        Tarjeta tarjetaSeleccionada = tblTarjetas.getSelectionModel().getSelectedItem();
+        if (tarjetaSeleccionada == null) {
+            mostrarAlerta("Advertencia", "Por favor selecciona una tarjeta para actualizar.");
             return;
         }
 
-        //String consultaSQL = "SELECT c.id, c.nombreCompleto, c.direccion, c.telefono, c.dui FROM TARJETAS T WHERE T.ID = ?";
-        String consultaSQL = "SELECT t.id, t.numeroTarjeta, t.fechaExpiracion, t.tipo, f.nombre, c.nombreCompleto FROM TARJETAS t\n" +
-                "INNER JOIN FACILITADORES f on t.facilitadorId = f.id\n" +
-                "INNER JOIN CLIENTES c on t.facilitadorId = c.id WHERE t.id = ?";
+        String numeroTarjeta = txtNoCompleto.getText();
+        String fechaExpiracion = txtFechaCaducidad.getText();
+        String tipo = comboBoxTipo.getSelectionModel().getSelectedItem();
+        String facilitador = comboBoxFacilitador.getSelectionModel().getSelectedItem();
+        int clienteId = Integer.parseInt(txtClienteId.getText());
 
-        try (Connection conexion = DriverManager.getConnection(jdbcUrl, usuario, contrasena);
-             PreparedStatement statement = conexion.prepareStatement(consultaSQL)) {
+        String consultaSQL = "UPDATE Tarjetas SET numeroTarjeta = ?, fechaExpiracion = ?, tipo = ?, facilitadorId = (SELECT id FROM Facilitadores WHERE nombre = ?), clienteId = ? " +
+                "WHERE id = ?";
 
-            statement.setString(1, idBuscado);
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, usuario, contrasena);
+             PreparedStatement statement = conn.prepareStatement(consultaSQL)) {
+
+            statement.setString(1, numeroTarjeta);
+            statement.setString(2, fechaExpiracion);
+            statement.setString(3, tipo);
+            statement.setString(4, facilitador);
+            statement.setInt(5, clienteId);
+            statement.setInt(6, tarjetaSeleccionada.getId());
+            statement.executeUpdate();
+
+            mostrarAlerta("Éxito", "Tarjeta actualizada exitosamente.");
+            cargarTarjetas();
+            limpiarCampos();
+        } catch (SQLException e) {
+            mostrarAlerta("Error", "Error al actualizar tarjeta: " + e.getMessage());
+        }
+    }
+
+    private void eliminarTarjeta() {
+        Tarjeta tarjetaSeleccionada = tblTarjetas.getSelectionModel().getSelectedItem();
+        if (tarjetaSeleccionada == null) {
+            mostrarAlerta("Advertencia", "Por favor selecciona una tarjeta para eliminar.");
+            return;
+        }
+
+        String consultaSQL = "DELETE FROM Tarjetas WHERE id = ?";
+
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, usuario, contrasena);
+             PreparedStatement statement = conn.prepareStatement(consultaSQL)) {
+
+            statement.setInt(1, tarjetaSeleccionada.getId());
+            statement.executeUpdate();
+
+            mostrarAlerta("Éxito", "Tarjeta eliminada exitosamente.");
+            cargarTarjetas();
+            limpiarCampos();
+        } catch (SQLException e) {
+            mostrarAlerta("Error", "Error al eliminar tarjeta: " + e.getMessage());
+        }
+    }
+
+    private void buscarTarjetaPorId() {
+        int idTarjeta = Integer.parseInt(txtBuscarIDTarjeta.getText());
+        String consultaSQL = "SELECT t.id, t.numeroTarjeta AS NumeroTarjeta, t.fechaExpiracion, t.tipo AS TipoTarjeta, f.nombre AS Facilitador, t.clienteId " +
+                "FROM TARJETAS t " +
+                "INNER JOIN Facilitadores f ON t.facilitadorId = f.id " +
+                "WHERE t.id = ?";
+
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, usuario, contrasena);
+             PreparedStatement statement = conn.prepareStatement(consultaSQL)) {
+
+            statement.setInt(1, idTarjeta);
             ResultSet resultado = statement.executeQuery();
 
-            listaTarjetas.clear();
-            while (resultado.next()) {
+            if (resultado.next()) {
                 Tarjeta tarjeta = new Tarjeta(
+                        resultado.getInt("id"),
+                        resultado.getString("clienteId"),
                         resultado.getString("numeroTarjeta"),
-                        resultado.getString("fechaExpiracion"),
-                        resultado.getString("tipo"),
-                        resultado.getString("facilitador"),
-                        resultado.getInt("idCliente")
+                        resultado.getString("Facilitador"),
+                        resultado.getString("fechaCaducidad"),
+                        resultado.getString("TipoTarjeta")
                 );
+                listaTarjetas.clear();
                 listaTarjetas.add(tarjeta);
+            } else {
+                mostrarAlerta("Información", "No se encontró ninguna tarjeta con el ID especificado.");
             }
         } catch (SQLException e) {
-            mostrarAlerta("Error", "Error al buscar cliente: " + e.getMessage());
+            mostrarAlerta("Error", "Error al buscar tarjeta: " + e.getMessage());
         }
     }
 
     private void cargarDatosCliente(Tarjeta tarjeta) {
-        txtNoCompleto.setText(tarjeta.getNumeroTarjeta());
-        //txtDireccion.setText(cliente.getDireccion());
-        //txtTelefono.setText(cliente.getTelefono());
+        txtNoCompleto.setText(tarjeta.getNoTarjeta());
+        txtFechaCaducidad.setText(tarjeta.getFechaCaducidad());
+        comboBoxTipo.getSelectionModel().select(tarjeta.getTipo());
+        comboBoxFacilitador.getSelectionModel().select(tarjeta.getFacilitador());
+        txtClienteId.setText(String.valueOf(tarjeta.getClienteId()));
 
         btnActualizar.setDisable(false);
         btnEliminar.setDisable(false);
-        btnAgregar.setDisable(true);
-    }
-
-    private void mostrarAlerta(String titulo, String mensaje) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(titulo);
-        alert.setHeaderText(null);
-        alert.setContentText(mensaje);
-        alert.showAndWait();
     }
 
     private void limpiarCampos() {
         txtNoCompleto.clear();
-        //txtDireccion.clear();
-        // txtTelefono.clear();
+        txtFechaCaducidad.clear();
+        comboBoxTipo.getSelectionModel().clearSelection();
+        comboBoxFacilitador.getSelectionModel().clearSelection();
+        txtClienteId.clear();
+        txtBuscarIDTarjeta.clear();
+
+        btnActualizar.setDisable(true);
+        btnEliminar.setDisable(true);
+    }
+
+    private void mostrarAlerta(String titulo, String mensaje) {
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setTitle(titulo);
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
     }
 }
